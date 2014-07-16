64e64aacd4d66b54e8a79d3d02ec42fc62889f5d
Jo-fai Chow <jofai.chow@gmail.com>
2014-07-16 11:24:15 +0100

    Updated to accommodate up to 10 odds

--- oddsimiser/server.R
+++ oddsimiser/server.R
@@ -5,7 +5,9 @@ suppressMessages(library(MCMCpack))
 
 shinyServer(function(input, output) {
   
+  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ## Optimiser (Single)
+  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   optimise_single <- reactive({
     
     odds_all <- c()  
@@ -13,14 +15,21 @@ shinyServer(function(input, output) {
     if (input$odds2 > 1) odds_all <- c(odds_all, input$odds2)
     if (input$odds3 > 1) odds_all <- c(odds_all, input$odds3)
     if (input$odds4 > 1) odds_all <- c(odds_all, input$odds4)
-    if (input$odds5 > 1) odds_all <- c(odds_all, input$odds5)    
+    if (input$odds5 > 1) odds_all <- c(odds_all, input$odds5)
+    if (input$odds6 > 1) odds_all <- c(odds_all, input$odds6)
+    if (input$odds7 > 1) odds_all <- c(odds_all, input$odds7)
+    if (input$odds8 > 1) odds_all <- c(odds_all, input$odds8)
+    if (input$odds9 > 1) odds_all <- c(odds_all, input$odds9)
+    if (input$odds10 > 1) odds_all <- c(odds_all, input$odds10)
     as.matrix(odds_all)
+    
         
     ## Evaluate Function
     eval_odds <- function(inp_p) {
       total_p <- sum(inp_p)
       rtn <- odds_all * as.matrix(inp_p) - total_p
-      return(min(rtn))
+      #return(min(rtn))
+      return(1-(max(rtn)-min(rtn))/max(rtn))
     }
     
     ## Define parameters for GA
@@ -32,7 +41,7 @@ shinyServer(function(input, output) {
     model <- ga(type = "real-valued",
                 fitness = eval_odds, monitor = FALSE,
                 min = para_ga_min, max = para_ga_max,
-                popSize = 20, maxiter = 1000)
+                popSize = 10, maxiter = 1000)
     
     ## Normalise and save results
     best_p <- summary(model)$solution / sum(summary(model)$solution)
@@ -52,7 +61,9 @@ shinyServer(function(input, output) {
   })
   
   
+  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ## Optimiser Double
+  ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   optimise_double <- reactive({
     
     odds_all <- c()  
@@ -60,7 +71,12 @@ shinyServer(function(input, output) {
     if (input$odds2 > 1) odds_all <- c(odds_all, input$odds2)
     if (input$odds3 > 1) odds_all <- c(odds_all, input$odds3)
     if (input$odds4 > 1) odds_all <- c(odds_all, input$odds4)
-    if (input$odds5 > 1) odds_all <- c(odds_all, input$odds5)    
+    if (input$odds5 > 1) odds_all <- c(odds_all, input$odds5)
+    if (input$odds6 > 1) odds_all <- c(odds_all, input$odds6)
+    if (input$odds7 > 1) odds_all <- c(odds_all, input$odds7)
+    if (input$odds8 > 1) odds_all <- c(odds_all, input$odds8)
+    if (input$odds9 > 1) odds_all <- c(odds_all, input$odds9)
+    if (input$odds10 > 1) odds_all <- c(odds_all, input$odds10)
     as.matrix(odds_all)
       
     games_all <- paste0("賽事", 1:length(odds_all))
@@ -73,7 +89,8 @@ shinyServer(function(input, output) {
     eval_odds <- function(inp_p) {
       total_p <- sum(inp_p)
       rtn <- odds_all * as.matrix(inp_p) - total_p
-      return(min(rtn))
+      #return(min(rtn))
+      return(1-(max(rtn)-min(rtn))/max(rtn))
     }
     
     ## Define parameters for GA
@@ -85,7 +102,7 @@ shinyServer(function(input, output) {
     model <- ga(type = "real-valued",
                 fitness = eval_odds, monitor = FALSE,
                 min = para_ga_min, max = para_ga_max,
-                popSize = 10, maxiter = 1000)
+                popSize = 20, maxiter = 2000)
     
     ## Normalise and save results
     best_p <- summary(model)$solution / sum(summary(model)$solution)
@@ -109,6 +126,8 @@ shinyServer(function(input, output) {
     t(sum_df)
     
   })
+  
+  
   
   ## Outputs
   
--- oddsimiser/ui.R
+++ oddsimiser/ui.R
@@ -13,26 +13,27 @@ shinyUI(fluidPage(
                     這個自動優化每個組合的最佳注碼, 以求達到最平衡的利潤.")),
       helpText(HTML("<br>")),
       
-      helpText(HTML("<h4>使用方法:</h4>假設你要平均投注於三至五場賽事, 輸入總投注額,
-                    然後輸入每場賽事的賠率, 再按'自動優化'. 預設顯示組合為'單拖', 可於標籤處選擇'二重'組合.")),
+      helpText(HTML("<h4>使用方法:</h4>假設你要平均投注於三至十場賽事, 輸入總投注額,
+                    然後輸入每場賽事的賠率, 再按'自動優化'. 預設顯示組合為'單拖', 可於標籤處選擇'二重彩'組合.")),
       helpText(HTML("<br>")),
                
       helpText(HTML("<h4>註:</h4>")),
-      helpText(HTML("1. 如果少於五場賽事，於多餘賽事上輸入賠率 1.")),
+      helpText(HTML("1. 如果少於十場賽事，於多餘賽事上輸入賠率 1.")),
       helpText(HTML("2. 利潤計法 - 保守假設只有單組合勝出.")),
+      helpText(HTML("3. 二重彩組合比較複雜，所需計算時間相對較長.")),
       helpText(HTML("<br>")),
       
       wellPanel(
         numericInput("stake", 
-                     label = "總投注額 ($)",
+                     label = "總投注額 (Total Stake) ($)",
                      value = 10),
         helpText(HTML("<br>")),
         
         numericInput("odds1", 
-                     label = "第一場賠率",
+                     label = "第一場賠率 (Game 1 Odds)",
                      value = 7.5),
         numericInput("odds2", 
-                     label = "第二場賠率",
+                     label = "第二場賠率 (etc ...)",
                      value = 8),
         numericInput("odds3", 
                      label = "第三場賠率",
@@ -43,18 +44,34 @@ shinyUI(fluidPage(
         numericInput("odds5", 
                      label = "第五場賠率",
                      value = 15),
+        numericInput("odds6", 
+                     label = "第六場賠率",
+                     value = 1),
+        numericInput("odds7", 
+                     label = "第七場賠率",
+                     value = 1),
+        numericInput("odds8", 
+                     label = "第八場賠率",
+                     value = 1),
+        numericInput("odds9", 
+                     label = "第九場賠率",
+                     value = 1),
+        numericInput("odds10", 
+                     label = "第十場賠率",
+                     value = 1),
+        
         
         helpText(HTML("<br>")),
         
         helpText(HTML("輸入完成後按這個")),
-        submitButton("自動優化")
+        submitButton("自動優化 (Optimise!)")
       )),
 
 
     mainPanel(
       tabsetPanel(
         tabPanel("單拖 (Single)", tableOutput("single")),
-        tabPanel("二重 (Double)", tableOutput("double"))
+        tabPanel("二重彩 (Double)", tableOutput("double"))
       )
     )
   )
